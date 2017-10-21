7cdc539ad7f3081f5125c39b786ff5bcc3460f49
Partha Sen <research@parthasen.net>
2017-10-21 12:19:55 +0530

    Create plotly_stream.py

---
+++ plotly_stream.py
@@ -0,0 +1,361 @@
+import zmq
+import plotly.plotly as ply
+import plotly.tools as plyt
+from plotly.graph_objs import *
+
+NO_TOKENS_ERROR = '''Can not find any streaming tokens, please 
+set existing tokens by running either 
+plotly.tools.set_credentials_file(user, api_key) or 
+plotly_stream.set_stream_tokens(token_list). 
+More on streaming tokens can be found on the Plotly documentation, 
+see https://plot.ly/python/streaming-tutorial'''
+
+NO_TOKENS_LEFT_ERROR = '''No stream token left, 
+to proceed add new token on your plotly account and run 
+plotly_stream.set_stream_tokens(token_list)'''
+
+NO_CRED_ERROR = '''No credentials file found, please run 
+plotly.tools.set_credentials_file(username, api_key).
+More on plotly credentials can be found on the Plotly documentation, 
+see https://plot.ly/python/streaming-tutorial'''
+
+
+class plotly_stream(object):
+    ''' A class for the easy implementation of streaming plots with Plotly.
+    '''
+    class_stream_tokens = list()
+
+    @classmethod
+    def set_stream_tokens(cls, tokens):
+        ''' Sets stream tokens as defined by the plotly setting page.
+        '''
+        cls.class_stream_tokens = tokens
+
+    def __init__(self, title, subplot_rows=1, subplot_columns=1,
+                 maxpoints=50, token_number=0):
+        ''' Returns a ploty_stream instance.
+
+        Parameters
+        ==========
+        title: str
+            the title of the plot
+        subplot_rows: int
+            number of subplot rows, default is 1
+        subplot_columns: int
+            number of subplot columns, default is 1
+        maxpoints: int
+            the number of points a trace contains, default is 50
+        token_number: int
+            the position of the first streaming token in the token list
+            used by the instance, default is 0
+            '''
+        self.user = plyt.get_credentials_file()['username']
+        self.auth_token = plyt.get_credentials_file()['api_key']
+        self.stream_tokens = plyt.get_credentials_file()['stream_ids']
+        if len(self.stream_tokens) == 0:
+            self.stream_tokens = self.class_stream_tokens
+            if len(self.stream_tokens) == 0:
+                raise IOError(NO_TOKENS_ERROR)
+        if self.user == "":
+            raise IOError(NO_CRED_ERROR)
+
+        self.sources = dict()
+        self.source_id = 0
+        self.traces = list()
+        self.data = list()
+        self.maxpoints = maxpoints
+        self.token_number = token_number
+        self.context = zmq.Context()
+        self.poller = zmq.Poller()
+        self.title = str(title)
+        self.cols = subplot_columns
+        self.rows = subplot_rows
+        self.subplots = dict()
+        self.layout = dict()
+        self.plot_url = ""
+        self.fig = None
+        for r in range(self.rows):
+            for c in range(self.cols):
+                self.subplots[r * self.cols + c] = subplot(r, c)
+        self.add_layout(layout={'title': self.title})
+
+    def init_plot(self, auto_open=False):
+        ''' Initializes the plot and returns the plot url,
+        is called by either start_streaming or get_plot_url
+        '''
+        if len(self.traces) == 0:
+            raise ValueError('No traces given')
+        specs = list()
+        titles = list()
+        for r in range(self.rows):
+            specs_r = list()
+            for c in range(self.cols):
+                specs_r.append(self.subplots[r * self.cols + c].spec)
+                titles.append(self.subplots[r * self.cols + c].title)
+            specs.append(specs_r)
+
+        self.fig = plyt.make_subplots(rows=self.rows, cols=self.cols,
+                                      specs=specs, subplot_titles=titles,
+                                      shared_xaxes=False,
+                                      shared_yaxes=False)
+        for target in self.layout:
+            if target == 'main':
+                self.fig['layout'].update(**self.layout[target])
+            else:
+                self.fig['layout'][target].update(**self.layout[target])
+
+        for r in range(self.rows):
+            specs_r = list()
+            for c in range(self.cols):
+                for t in self.subplots[r * self.cols + c].traces:
+                    self.fig.append_trace(t.trace, r + 1, c + 1)
+        self.plot_url = ply.plot(
+            self.fig, 'plotly_stream', auto_open=auto_open)
+
+    def add_layout(self, target="", layout={}):
+        ''' Adds layout information of the plot.
+
+        Parameters
+        ==========
+        target: object
+            the object, the layout should be applied one; if empty,
+            the whole plot is set as target
+        layout: dict
+            a dictionary containing the key/value pairs of the new layout
+        '''
+        if target == "":
+            target = 'main'
+        if target not in self.layout:
+            self.layout[target] = layout
+        else:
+            self.layout[target].update(layout)
+
+    def add_data_source(self, host, port):
+        '''Adds a datasource to the instance'''
+        socket = self.add_socket(host, port)
+        socket.setsockopt_string(zmq.IDENTITY, str(self.source_id))
+
+        self.sources[self.source_id] = socket
+        self.source_id += 1
+        self.poller.register(socket, zmq.POLLIN)
+        return socket
+
+    def add_trace(self, kind, data_source, name=None, row=1,
+                  col=1, parse_data=None):
+        ''' Adds a new trace object to a subplot.
+
+        Parameters
+        ==========
+        kind: str
+            the kind of graphical object, one of "scatter" or "bars"
+        name: str
+            the name of the trace, appears for example in the legend
+        data_source: object
+            a data source object as defined by plotly_stream.add_data_source
+        row and col: ints
+            the subplot to append the trace object to
+        parse_data: object
+            an optional callback function to parse the data recieved
+            from a server; the function must accept a string
+            (the incoming message) and returns the x and y data
+        '''
+
+        if self.token_number >= len(self.stream_tokens):
+            raise ValueError(NO_TOKENS_LEFT_ERROR)
+        if name is None:
+            name = "Stream %s" % self.stream_tokens[self.token_number]
+        if kind == "scatter":
+            trace = stream_trace(self.stream_tokens[self.token_number],
+                                 data_source, name, self.maxpoints,
+                                 (row - 1) * self.cols + col - 1, parse_data)
+        elif kind == 'bars':
+            trace = stream_bars(self.stream_tokens[self.token_number],
+                                data_source, name, self.maxpoints,
+                                (row - 1) * self.cols + col - 1, parse_data)
+        self.token_number += 1
+        self.traces.append(trace)
+        self.data.append(trace.trace)
+        self.subplots[(row - 1) * self.cols + col - 1].append_trace(trace)
+
+        return trace
+
+    def set_max_points(self, maxpoints):
+        ''' Sets the maximum number of points plotted by each trace
+        '''
+        if type(maxpoints) != int:
+            raise TypeError("maxpoints must be of type int")
+        elif maxpoint > 0:
+            self.maxpoints = maxpoints
+
+    def get_plot_url(self):
+        ''' Returns the plot url, must be called after all other settings
+        '''
+        if self.plot_url == "":
+            self.init_plot()
+        return self.plot_url
+
+    def start_streaming(self, auto_open=True):
+        ''' Starts streaming, must be called after all other settings
+
+        Parameters
+        ==========
+        auto_open: boolean
+            if True (default) the method tries to open a browser
+            with the new plot
+        '''
+        if self.plot_url == "":
+            self.init_plot()
+            print("Plot url: %s" % self.plot_url)
+
+        for trace in self.traces:
+            trace.stream.open()
+        while True:
+            try:
+                socks = dict(self.poller.poll())
+            except KeyboardInterrupt:
+                break
+
+            for socket in socks:
+                socket_id = int(socket.getsockopt_string(zmq.IDENTITY))
+                msg = socket.recv_string()
+
+                for trace in self.traces:
+                    if trace.data_source == socket_id:
+                        x, y = trace.parse_data(msg)
+                        trace.write_to_stream({'x': x, 'y': y})
+                        for t in self.subplots[trace.subplot_id].traces:
+                            if t.data_source != socket_id:
+                                t.write_to_stream({'x': x, 'y': ' '})
+
+    def add_socket(self, host, port):
+        ''' Add a new socket
+        '''
+        socket = self.context.socket(zmq.SUB)
+        socket.connect('tcp://%s:%s' % (host, port))
+        socket.setsockopt_string(zmq.SUBSCRIBE, u'')
+        return socket
+
+    def set_subplot_title(self, r, c, title):
+        '''Sets the title of a subplot'''
+        self.subplots[(r - 1) * self.cols + c - 1].title = title
+
+    def set_subplot_specs(self, r, c, spec):
+        '''Sets the spec of a subplot'''
+        self.subplots[(r - 1) * self.cols + c - 1].spec = spec
+
+
+class DataSource(object):
+    ''' Class to generate the data sources used by plotly_stream
+    '''
+
+    def __init__(self, host, port):
+        context = zmq.Context()
+        self.socket = context.socket(zmq.SUB)
+        self.socket.connect('tcp://%s:%s' % (host, port))
+        self.socket.setsockopt_string(zmq.SUBSCRIBE, u'')
+
+    def get_data(self):
+        return self.socket.recv_string()
+
+
+class stream_trace(object):
+    ''' Class to instantiate the streamed scatter objects used by plotly_stream
+    '''
+
+    def parse_data(cls, msg):
+        ''' Default callback function for streamed scatter objects,
+        used if no other callback is given
+        '''
+        parts = msg.split(',')
+        x = str(parts[0])
+        y = float(parts[1])
+        return x, y
+
+    def __init__(self, stream_token, data_source, name,
+                 maxpoints, subplot_id, parse_data=None):
+        self.data_source = int(data_source.getsockopt_string(zmq.IDENTITY))
+        self.name = name
+        self.trace = Scatter(x=[], y=[],
+                             stream=dict(token=stream_token,
+                                         maxpoints=maxpoints),
+                             name=name, mode='lines+markers', connectgaps=True)
+        self.stream = ply.Stream(stream_token)
+        self.subplot_id = subplot_id
+        if parse_data is not None:
+            self.parse_data = parse_data
+
+    def open_stream(self):
+        self.stream.open()
+
+    def write_to_stream(self, data):
+        self.stream.write(data)
+
+    def close_stream(self):
+        self.stream.close()
+
+    def set_parse_data(self, callback):
+        self.parse_data = callback
+
+
+class stream_bars(object):
+    ''' Class to generate the streamed bars objects used by plotly_stream
+    '''
+
+    def __init__(self, stream_token, data_source, name,
+                 maxpoints, subplot_id, parse_data=None):
+        self.data_source = int(data_source.getsockopt_string(zmq.IDENTITY))
+        self.name = name
+        self.bar_labels = list()
+        self.trace = Bar(x=[], y=[],
+                         stream=dict(token=stream_token, maxpoints=maxpoints),
+                         name=name)
+        self.stream = ply.Stream(stream_token)
+        self.subplot_id = subplot_id
+        if parse_data is not None:
+            self.parse_data = parse_data
+
+    def parse_data(self, msg):
+        ''' Default callback function for streamed bars objects,
+        used if no other callback is given
+        '''
+        parts = msg.split(",")
+        y = [float(p) for p in parts]
+        if len(self.bar_labels) == len(y):
+            x = self.bar_labels
+        else:
+            x = [str(i + 1) for i in range(len(y))]
+        return x, y
+
+    def open_stream(self):
+        self.stream.open()
+
+    def write_to_stream(self, data):
+        data.update({'type': 'bar'})
+        self.stream.write(data)
+
+    def close_stream(self):
+        self.stream.close()
+
+    def set_parse_data(self, callback):
+        self.parse_data = callback
+
+
+class subplot(object):
+    ''' Class to instantiate subplot object as used by ploty_stream
+    '''
+
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.spec = dict()
+        self.title = ""
+        self.traces = list()
+
+    def set_title(self, title):
+        self.title = title
+
+    def set_spec(self, spec):
+        self.spec = spec
+
+    def append_trace(self, trace):
+        self.traces.append(trace)
